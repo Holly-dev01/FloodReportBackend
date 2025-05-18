f3ca9995263739ad3a765e784faed477bf562ade
Holly-dev01 <shimaholly1@gmail.com>
2025-05-18 19:08:41 +0200

    initial springboot project

---
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/FloodReportingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FloodReportingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodReportingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodReportingAppApplication.class, args);
+	}
+
+}
---
+++ FloodReportingApp/src/test/java/com/example/FloodReportingApp/FloodReportingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FloodReportingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodReportingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
