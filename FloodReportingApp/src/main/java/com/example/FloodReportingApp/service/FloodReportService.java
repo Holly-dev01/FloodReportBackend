57840400b5d587bb15cc917ccf48dd4c5732c040
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 10:00:08 +0200

    Update FloodReportService.java
    pour utiliser le DTO

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/service/FloodReportService.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/service/FloodReportService.java
@@ -1,6 +1,7 @@
 package com.example.FloodReportingApp.service;
 
 import com.example.FloodReportingApp.repository.FloodReportRepository;
+import com.example.FloodReportingApp.dto.FloodReportDTO;
 import com.example.FloodReportingApp.model.FloodReport;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +26,16 @@ public class FloodReportService {
     public List<FloodReport> getReportsByDeviceId(String deviceId) {
       return reportRepository.findByDeviceId(deviceId);
     }
+
+  public FloodReport createReport(FloodReportDTO reportDTO) {
+    FloodReport report = new FloodReport();
+    report.setType(reportDTO.getType());
+    report.setDescription(reportDTO.getDescription());
+    report.setLatitude(reportDTO.getLatitude());
+    report.setLongitude(reportDTO.getLongitude());
+    report.setTimestamp(new Date());
+    report.setDeviceId(reportDTO.getDeviceId());
+    
+    return reportRepository.save(report);
+  }
 }
