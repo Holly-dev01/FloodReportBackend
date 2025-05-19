17fce984092c7ff3c0456319341e32f11c5a2da6
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 09:13:14 +0200

    Ajout de l'endpoint GET pour récupérer tous les rapports

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/controller/FloodReportController.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/controller/FloodReportController.java
@@ -1,6 +1,7 @@
 package com.example.FloodReportingApp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.FloodReportingApp.model.FloodReport;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/reports")
 public class FloodReportController {
-    
-@PostMapping
-public ResponseEntity<FloodReport> createReport(@RequestBody FloodReport report) {
+
+  @PostMapping
+  public ResponseEntity<FloodReport> createReport(@RequestBody FloodReport report) {
     FloodReport savedReport = reportService.createReport(report);
     return ResponseEntity.ok(savedReport);
-}
+   }
+   @GetMapping
+   public ResponseEntity<List<FloodReport>> getAllReports() {
+     List<FloodReport> reports = reportService.getAllReports();
+     return ResponseEntity.ok(reports);
+    }
 }
