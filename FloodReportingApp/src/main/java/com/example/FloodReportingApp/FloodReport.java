cc5a241aa0d66ab1ffca97625461580786263dca
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 08:30:01 +0200

    ajout de la classe de modèle FloodReport de base

---
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/FloodReport.java
@@ -0,0 +1,43 @@
+package com.example.FloodReportingApp;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Document(collection = "flood_reports")
+public class FloodReport {
+    @Id
+    private String id;
+    private String type;
+    private String description;
+    private double latitude;
+    private double longitude;
+    private Date timestamp;
+
+   public FloodReport(String type, String description, double latitude, double longitude, Date timestamp, String deviceId, String severity) {
+        this.type = type;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+
+    public Date getTimestamp() { return timestamp; }
+    public void setTimestamp(Date timestamp) { this.timestamp = timestamp; }
+
+}
