5c1149151710495e11ac4070a41ae4c886d6a9c1
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 09:37:04 +0200

    Ajout de la méthode de recherche par deviceId dans le repository

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/repository/FloodReportRepository.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/repository/FloodReportRepository.java
@@ -2,7 +2,9 @@ package com.example.FloodReportingApp.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FloodReportRepository extends MongoRepository<FloodReport, String>{
-
+    List<FloodReport> findByDeviceId(String deviceId);
 }
