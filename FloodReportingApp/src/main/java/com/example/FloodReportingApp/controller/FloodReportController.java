a0a7d53de2401a6aef8f110456a71c19669a771c
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 09:06:02 +0200

    Ajout de l'endpoint POST pour créer un rapport

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/controller/FloodReportController.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/controller/FloodReportController.java
@@ -1,11 +1,21 @@
 package com.example.FloodReportingApp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.FloodReportingApp.model.FloodReport;
 
 
 @RestController
 @RequestMapping("/api/reports")
 public class FloodReportController {
     
+@PostMapping
+public ResponseEntity<FloodReport> createReport(@RequestBody FloodReport report) {
+    FloodReport savedReport = reportService.createReport(report);
+    return ResponseEntity.ok(savedReport);
+}
 }
