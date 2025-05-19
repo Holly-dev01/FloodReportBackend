b73225374cfb398d4a5fe7684a216c48116ff0af
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 08:49:47 +0200

    Create FloodReportController.java
    creation du contreolleur avec point de base

---
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/controller/FloodReportController.java
@@ -0,0 +1,11 @@
+package com.example.FloodReportingApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/reports")
+public class FloodReportController {
+    
+}
