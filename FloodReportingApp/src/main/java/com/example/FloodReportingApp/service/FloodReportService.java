b0f79cede244ce6b7785c647347821bf6dbeed86
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 09:09:23 +0200

    Implémentation de la récupération de tous les rapports

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/service/FloodReportService.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/service/FloodReportService.java
@@ -7,14 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class FloodReportService {
     @Autowired
     private FloodReportRepository reportRepository;
     
-public FloodReport createReport(FloodReport report) {
-    report.setTimestamp(new Date());
-    return reportRepository.save(report);
-}
+    public FloodReport createReport(FloodReport report) {
+      report.setTimestamp(new Date());
+     return reportRepository.save(report);
+     }
+
+    public List<FloodReport> getAllReports() {
+      return reportRepository.findAll();
+    }  
 }
