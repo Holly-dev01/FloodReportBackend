6e4914aba2d6ce1b261d34f93231035a831cc224
Holly-dev01 <shimaholly1@gmail.com>
2025-05-19 09:55:10 +0200

    adding an importation
    j'avais oubliee d'importer la classe d'ou l'erreur

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/repository/FloodReportRepository.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/repository/FloodReportRepository.java
@@ -1,6 +1,7 @@
 package com.example.FloodReportingApp.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import com.example.FloodReportingApp.model.FloodReport;
 
 import java.util.List;
 
