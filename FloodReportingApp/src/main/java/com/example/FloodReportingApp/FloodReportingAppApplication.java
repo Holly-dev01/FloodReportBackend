79d12b1b7a91bd53985aaf8b2f5e26e158f801a7
Holly-dev01 <shimaholly1@gmail.com>
2025-05-23 10:22:59 +0200

    add default admin initialization on startup

--- FloodReportingApp/src/main/java/com/example/FloodReportingApp/FloodReportingAppApplication.java
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/FloodReportingAppApplication.java
@@ -1,13 +1,28 @@
 package com.example.FloodReportingApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.FloodReportingApp.service.AuthService;
 
 @SpringBootApplication
 public class FloodReportingAppApplication {
 
+    @Autowired
+    private AuthService authService;
 	public static void main(String[] args) {
 		SpringApplication.run(FloodReportingAppApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner initData() {
+        return args -> {
+            // Initialiser l'admin par défaut
+            authService.initDefaultAdmin();
+        };
+    }
 
 }
