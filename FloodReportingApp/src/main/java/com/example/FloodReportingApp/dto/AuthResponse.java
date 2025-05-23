8ac401cd068134a132cc460569e4d05cd490b580
Holly-dev01 <shimaholly1@gmail.com>
2025-05-23 09:16:50 +0200

    Add authentification DTO for response

---
+++ FloodReportingApp/src/main/java/com/example/FloodReportingApp/dto/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.example.FloodReportingApp.dto;
+
+public class AuthResponseDTO {
+    private boolean success;
+    private String token;
+    private String message;
+    
+    public AuthResponseDTO() {}
+    
+    public AuthResponseDTO(boolean success, String token, String message) {
+        this.success = success;
+        this.token = token;
+        this.message = message;
+    }
+
+    public boolean isSuccess() { return success; }
+    public void setSuccess(boolean success) { this.success = success; }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
